ef248908724e566476b3f59e1779b0475247fc25
Chris Ro <christopher.j.ro@gmail.com>
2018-05-17 01:58:25 -0700

    Initial SCWS corpus retrieval

---
+++ utilities/get_scws_corpus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Make the data directory, get the data from the internet, unzip, and save.
+# The results are stored in ../data/SCWS.
+mkdir -p ../data
+curl -J -L http://nlp.stanford.edu/data/WestburyLab.wikicorp.201004.txt.bz2 -o ../data/SCWS/WestburyLab.wikicorp.201004.txt.bz2
+bunzip2 ../data/SCWS/WestburyLab.wikicorp.201004.txt.bz2 -d
+
