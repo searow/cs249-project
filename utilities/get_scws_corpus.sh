629025e1362a83f51c25fd89fabc36f9a224e4b4
Chris Ro <christopher.j.ro@gmail.com>
2018-05-25 20:10:08 -0700

    Fix directory creation for SCWS

--- utilities/get_scws_corpus.sh
+++ utilities/get_scws_corpus.sh
@@ -3,6 +3,7 @@
 # Make the data directory, get the data from the internet, unzip, and save.
 # The results are stored in ../data/SCWS.
 mkdir -p ../data
+mkdir -p ../data/SCWS
 curl -J -L http://nlp.stanford.edu/data/WestburyLab.wikicorp.201004.txt.bz2 -o ../data/SCWS/WestburyLab.wikicorp.201004.txt.bz2
 bunzip2 ../data/SCWS/WestburyLab.wikicorp.201004.txt.bz2 -d
 
