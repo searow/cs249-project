56942f6aacbdd652ad675bf0cb016109b52f6f2a
Chris Ro <christopher.j.ro@gmail.com>
2018-05-26 22:13:06 -0700

    Add port forwarding for tensorboard

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,5 @@
 # Starts docker container on host port 8888, mounts the current directory,
 # which should be called from the project root directory, into the container
 # at /notebooks/cs249.
-docker run -it -p 8888:8888 -v $PWD:/notebooks/cs249 searow/cs249
+docker run -it -p 8888:8888 -p 6006:6006 -v "$PWD:/notebooks/cs249" searow/cs249
 
