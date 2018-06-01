334f032fcac344cc4c8de55ee08568775bf050cc
David Rangel <drangelalarcon@gmail.com>
2018-06-01 13:11:45 -0700

    plots global/local fit, target/context words, and only words above a training threshold

--- mapembs_pca.py
+++ mapembs_pca.py
@@ -18,18 +18,23 @@ import pickle
 from sklearn.decomposition import PCA
 import matplotlib
 import matplotlib.pyplot as plt
-from utils import load_as_dict
-import pandas as pd
+from utils import load_as_dict, filter_words
 
 # Hardcoded Elements:
-k = 3
-folder_name = '/home/david/Documents/2018SPRING/249/cs249-project/output/models_k_3_dim_100_neg_10_swind_5_2018-05-31-15:53:01'
+tgt_k = 3
+nce_k = 1
+folder_name = '/home/david/Documents/2018SPRING/249/cs249-project/output/models_k_3_dim_100_neg_65_swind_5_2018-05-31-23:37:08'
 
 corpus = 'data/word2vec_sample/text8_tokenized_50000'
-words = ['king', 'queen', 'actor', 'actress', 'father', 'mother']#'apple', 'banana', 'pear', 'microsoft', 'google'] 
+words = ['china', 'beijing', 'japan', 'tokyo', 'spain', 'madrid', 'germany', 'berlin', 'turkey', 'ankara']
+#'migrant', 'reminded', 'obtaining', 'confused', 'subdivision', 'reich']#'apple', 'banana', 'pear', 'microsoft', 'google'] 
+#'king', 'queen', 'actor', 'actress', 'father', 'mother',
 
-prefix_tgt = 'pca_plotof_tgt'
-prefix_nce = 'pca_plotof_ctx'
+prefix = 'pca_plot_'
+prefix_lcl_tgt = prefix + 'lclfit_tgt_'
+prefix_lcl_nce = prefix + 'lclfit_ctx_'
+prefix_glb_tgt = prefix + 'glbfit_tgt_'
+prefix_glb_nce = prefix + 'glbfit_ctx_'
 
 # Make sure that the corpus file exists before continuing.
 if not path.isdir(folder_name):
@@ -42,6 +47,41 @@ def create_plotID_list(words, dictionary):
 def set_plot_defaults():
 	font = {'size'   : 25}
 	matplotlib.rc('font', **font)
+
+def get_embs_labels(k, allembs, allcnts, reverse_dict, plot_only):
+	embs_list = []
+	labels_list = []
+	norm_cnts, mask = filter_words(allcnts, 0.25)
+	is_untrained_embs = np.count_nonzero(norm_cnts == 0)
+
+	for i in range(k):
+		embs = []
+		labels = []
+
+		for j in plot_only:
+			if(mask[j][i] or is_untrained_embs):
+				embs.append(allembs[i][j, :])
+
+		embs_list.append(embs)
+
+		if k == 1:
+			for j in plot_only:
+				if(mask[j][i] or is_untrained_embs):
+					labels.append(
+						reverse_dict[j]+'_{}'.format(int(allcnts[j][i])))
+		else:
+			for j in plot_only:
+				if(mask[j][i] or is_untrained_embs):
+					labels.append(reverse_dict[j]+'_'+str(i) \
+						+ '_{}'.format(int(allcnts[j][i])))
+
+		labels_list += labels
+
+	return embs_list, labels_list
+
+def plot_with_built_labels(low_dim_embs, labels, folder_name, \
+							prefix, embFileN):
+	plot_with_labels(low_dim_embs, labels, folder_name + '/' + prefix + embFileN + '.png')
 
 # Function to draw visualization of distance between embeddings.
 def plot_with_labels(low_dim_embs, labels, filename):
@@ -59,9 +99,9 @@ def plot_with_labels(low_dim_embs, labels, filename):
 				va='bottom',
 				fontsize=35)
 
-		axes = plt.gca()
-		axes.set_xlim([-1,1])
-		axes.set_ylim([-1,1])
+		# axes = plt.gca()
+		# axes.set_xlim([-1,1])
+		# axes.set_ylim([-1,1])
 
 	plt.savefig(filename)
 	plt.close()
@@ -74,14 +114,19 @@ try:
 	dictionary = read_obj['dictionary']
 	reversed_dictionary = read_obj['reversed_dictionary']
 
-	pca = PCA(n_components=2)
+	pca_lcl_tgt = PCA(n_components=2)
+	pca_lcl_nce = PCA(n_components=2)
+	pca_glb_tgt = PCA(n_components=2)
+	pca_glb_nce = PCA(n_components=2)
 	plot_only = create_plotID_list(words, dictionary)
 	
 	print('Plotting...')
 	for bf in listdir(folder_name):
 		# Check for existing plot files
-		if bf.startswith(prefix_tgt) or bf.startswith(prefix_nce):
+		if bf.startswith(prefix):
 			continue
+
+		print('Plotting {}'.format(bf))
 
 		# Load vectors
 		loaded_data = load_as_dict(folder_name + '/' + bf)
@@ -90,50 +135,41 @@ try:
 		nce_embs = loaded_data['context_embeddings']
 		nce_cnts = loaded_data['context_counts']
 
-		low_dim_embs_list = []
-		labels_list = []
+		# Fit the pca to all embeddings
+		pca_glb_tgt.fit(np.concatenate(tgt_embs))
+		pca_glb_nce.fit(np.concatenate(nce_embs))
 
-		# Condition embeddings/labels
-		for i in range(k):
-			low_dim_embs = tgt_embs[i][plot_only, :]
-			low_dim_embs_list.append(low_dim_embs)
-			if k == 1:
-				labels = [reversed_dictionary[j] for j in plot_only]
-			else:
-				labels = [reversed_dictionary[j] + '_' + str(i) \
-					+ '_{}'.format(int(tgt_cnts[j][i])) \
-					for j in plot_only]
-			labels_list += labels
+		# Plot target Words
+		high_dim_embs_list, labels_list = get_embs_labels(
+				tgt_k, tgt_embs, tgt_cnts, reversed_dictionary, plot_only)
 
-		# import pdb; pdb.set_trace()
-		low_dim_embs_list = pca.fit_transform(np.concatenate(low_dim_embs_list))
+		pca_lcl_tgt.fit(np.concatenate(high_dim_embs_list))
 
-		#Plot
-		plot_with_labels(
-			low_dim_embs_list, labels_list, \
-			folder_name + '/' + prefix_tgt + bf + '.png')
+		low_dim_embs_list = pca_lcl_tgt.transform(
+				np.concatenate(high_dim_embs_list))
+		plot_with_built_labels(
+				low_dim_embs_list, labels_list, folder_name, prefix_lcl_tgt, bf)
 
-		low_dim_embs_list = []
-		labels_list = []
+		low_dim_embs_list = pca_glb_tgt.transform(
+				np.concatenate(high_dim_embs_list))
+		plot_with_built_labels(
+				low_dim_embs_list, labels_list, folder_name, prefix_glb_tgt, bf)
 
-		# Condition embeddings/labels
-		for i in range(k):
-			low_dim_embs = nce_embs[i][plot_only, :]
-			low_dim_embs_list.append(low_dim_embs)
-			if k == 1:
-				labels = [reversed_dictionary[j] for j in plot_only]
-			else:
-				labels = [reversed_dictionary[j] + '_' + str(i) \
-					+ '_{}'.format(int(nce_cnts[j][i])) \
-					for j in plot_only]
-			labels_list += labels
+		# Plot context Words
+		high_dim_embs_list, labels_list = get_embs_labels(
+				nce_k, nce_embs, nce_cnts, reversed_dictionary, plot_only)
+		
+		pca_lcl_nce.fit(np.concatenate(high_dim_embs_list))
 
-		low_dim_embs_list = pca.fit_transform(np.concatenate(low_dim_embs_list))
+		low_dim_embs_list = pca_lcl_nce.transform(
+				np.concatenate(high_dim_embs_list))
+		plot_with_built_labels(
+				low_dim_embs_list, labels_list, folder_name, prefix_lcl_nce, bf)
 
-		#Plot
-		plot_with_labels(
-			low_dim_embs_list, labels_list, \
-			folder_name + '/' + prefix_nce + bf + '.png')
+		low_dim_embs_list = pca_glb_nce.transform(
+				np.concatenate(high_dim_embs_list))
+		plot_with_built_labels(
+				low_dim_embs_list, labels_list, folder_name, prefix_glb_nce, bf)
 
 	print('Finished Plotting!')
 
