5a1f93e6b23f26cd82b0f49eb60ff9e4e31ea838
Chris Ro <christopher.j.ro@gmail.com>
2018-06-09 00:15:32 -0700

    Add counts

--- projector.py
+++ projector.py
@@ -1,7 +1,7 @@
 from utils import *
 
-match_list = ['apple', 'microsoft', 'google', 'banana', 'pear']
-embedding_filepath = 'projector-test/saved_embeddings_step_17000000_k_2_dim_150_neg_10_swind_5_contexts_1_2018-06-06-23-20-25'
+match_list = ['apple', 'microsoft', 'google', 'banana', 'pear', 'bank', 'river', 'money', 'right', 'left', 'correct', 'wrong']
+embedding_filepath = 'projector-test/saved_embeddings_step_5666667_k_2_dim_150_neg_10_swind_5_contexts_1_2018-06-06-23-20-25'
 corpus_filepath = 'data/enwik9/enwik9_tokenized_50000'
 output_embeddings_filepath = 'projector_embeddings.tsv'
 output_metadata_filepath = 'projector_metadata.tsv'
@@ -11,6 +11,7 @@ embedding_data_load = load_as_dict(embedding_filepath)
 corpus_data_load = load_as_dict(corpus_filepath)
 
 targets = embedding_data_load['target_embeddings']
+train_counts = embedding_data_load['target_counts']
 token_to_name = corpus_data_load['reversed_dictionary']
 
 print('Writing to file')
@@ -22,4 +23,4 @@ with open(output_embeddings_filepath, 'w') as embed_file:
                     for embed_idx in range(targets.shape[2]):
                         embed_file.write('{}\t'.format(targets[k_val, vocab_idx, embed_idx]))
                     embed_file.write('\n')
-                    meta_file.write('{}_{}\n'.format(token_to_name[vocab_idx], k_val))
+                    meta_file.write('{}_{}_{}\n'.format(token_to_name[vocab_idx], k_val, train_counts[vocab_idx][k_val]))
